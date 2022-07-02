de8a26f79fd45d4a1bcdfe12348e4aafa7a0c3bb
lagiewnik <lagiewnik@gmail.com>
2022-07-02 22:05:26 +0200

    first lessons

---
+++ lekcje_JS/03_loops.js
@@ -0,0 +1,18 @@
+//for
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+    console.log(Math.random());
+}
+
+
+//while
+let i =0
+while (i <12) {
+    console.log("while "+i);
+    i++;
+}
+
+//do while
+do {
+    
+} while (condition);
