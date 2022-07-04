e8ce052d15db106d9dd2dabaf050e700665cc22d
lagiewnik <lagiewnik@gmail.com>
2022-07-04 13:22:08 +0200

    checkbox handling

---
+++ cypress/integration/06_checkbox.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe("E2E - checkbox handling", () => {
+    beforeEach('', () => {
+        cy.visit('/');
+    })
+
+    it('checkbox', () => {
+        cy.visit('/index.php?id_category=3&controller=category');
+        cy.get('#ul_layered_category_0').find("input").then(checkboxes => {
+            cy.get(checkboxes).eq(0).check();
+            cy.get(checkboxes).eq(1).check({ force: true }).invoke("prop", "checked").then(checked => {
+                cy.log(checked);
+            });
+        })
+        //Multiple checked for all finded input
+        cy.get("#ul_layered_id_attribute_group_1").find("input").check();
+    })
+
+});
